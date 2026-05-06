1f7deeb892872d89ced4ebc3c8461aa462882b3e
Henrique_GG <ggoncalveshenrique@gmail.com>
2026-05-06 12:18:14 -0300

    feat: application

--- src/main/java/com/synergymarket/SynergyMarketApplication.java
+++ src/main/java/com/synergymarket/SynergyMarketApplication.java
@@ -1,1 +1,11 @@
-// Arquivo principal criado
+package com.synergymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergyMarketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SynergyMarketApplication.class, args);
+    }
+}
